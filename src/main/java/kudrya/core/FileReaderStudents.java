742c85031eeac755f734d59806e67886773d1093
Anton Kudryavtsev <kam123ua@gmail.com>
2019-01-24 00:35:21 +0200

    Add reader

---
+++ src/main/java/kudrya/core/FileReaderStudents.java
@@ -0,0 +1,41 @@
+package kudrya.core;
+
+import kudrya.core.writer.FileWriter;
+import org.apache.log4j.Logger;
+
+import java.io.*;
+import java.util.ArrayList;
+import java.util.List;
+
+public class FileReaderStudents implements ReaderStudents {
+
+    final static Logger logger = Logger.getLogger(FileReaderStudents.class);
+
+    private String path;
+
+    public FileReaderStudents(String path) {
+        this.path = path;
+    }
+
+    @Override
+    public List<Student> get() {
+        FileInputStream fileInputStream = null;
+        try {
+            fileInputStream = new FileInputStream(path);
+        } catch (FileNotFoundException e) {
+            logger.error("Error in writing object to" + path);
+        }
+        BufferedInputStream bufferedInputStream = new BufferedInputStream(fileInputStream);
+        ObjectInputStream objectInputStream = null;
+        List<Student> res = new ArrayList<Student>();
+        try {
+            objectInputStream = new ObjectInputStream(bufferedInputStream);
+            res = (List<Student>) objectInputStream.readObject();
+            objectInputStream.close();
+        } catch (IOException e) {
+            logger.error("Error in getting object from" + path);
+        } catch (ClassNotFoundException e) {
+        }
+        return res;
+    }
+}
---
+++ src/main/java/kudrya/core/ReaderStudents.java
@@ -0,0 +1,7 @@
+package kudrya.core;
+
+import java.util.List;
+
+public interface ReaderStudents {
+    List<Student> get();
+}
